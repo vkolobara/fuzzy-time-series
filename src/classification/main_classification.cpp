5be2c8d36e7d3a13b15d03139f9d21ed63498361
Vinko Kolobara <kolobara.vinko@gmail.com>
2018-06-02 13:59:48 +0200

    Working example for training/testing

--- src/classification/main_classification.cpp
+++ src/classification/main_classification.cpp
@@ -6,9 +6,11 @@
 #include "ClassifierFTSEvalOp.h"
 #include "../regression/RegressionLoggingOperator.h"
 #include "../regression/RegressionFTSEvalOp.h"
+#include "ClassificationLoggingOperator.h"
 
 int main(int argc, char **argv) {
     StateP state (new State);
+    state->addOperator((ClassificationLoggingOperatorP) new ClassificationLoggingOperator);
     state->setEvalOp((ClassifierFTSEvalOpP) new ClassifierFTSEvalOp);
 
     state->initialize(argc, argv);
